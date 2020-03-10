d8146ceb38621f8224129a77065b7009ae2ac9cc
Teguh <teguhpanjaitan@gmail.com>
2020-03-10 11:18:29 +0700

    Add new field to user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,17 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('user_role');
+            $table->string('login_id');
             $table->string('email')->unique();
+            $table->string('name');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->integer('updated_by');
+            $table->string('remarks');
+            $table->tinyInteger('locked');
         });
     }
 
